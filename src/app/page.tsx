e246dc6ffdf65e22ecb531e66769417a20211a4d
Олег Дидечкин <crm.29431@dz.com>
2023-11-04 19:17:40 +0200

    Font setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import { FC } from 'react';
 type PageProps = {};
 
 const Page: FC<PageProps> = ({}) => {
-  return <div>page</div>;
+  return (
+    <section className='flex flex-1'>
+      <p className='text-xl'>Hello world</p>
+    </section>
+  );
 };
 
 export default Page;
