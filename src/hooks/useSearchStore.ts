02bb95f1561ba5842409791a07991f8317ba74b6
Олег Дидечкин <crm.29431@dz.com>
2023-11-05 23:58:35 +0200

    Search clear button

--- src/hooks/useSearchStore.ts
+++ src/hooks/useSearchStore.ts
@@ -3,11 +3,13 @@ import { create } from 'zustand';
 type SearchStateType = {
   search: string;
   setSearch: (search: string) => void;
+  clearSearch: () => void;
 };
 
 const useSearchStore = create<SearchStateType>((set) => ({
   search: '',
   setSearch: (search) => set(() => ({ search: search })),
+  clearSearch: () => set(() => ({ search: '' })),
 }));
 
 export default useSearchStore;
