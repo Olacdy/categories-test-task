fd0ad276ffc26b7f4bcf73b9c310bd0c6e853cb4
Олег Дидечкин <crm.29431@dz.com>
2023-11-05 18:13:13 +0200

    Prettified files

--- src/components/apply-changes.tsx
+++ src/components/apply-changes.tsx
@@ -16,7 +16,7 @@ const ApplyChanges: FC<ApplyChangesProps> = ({
   handleCancelChanges,
 }) => {
   return (
-    <div className='bg-apply-changes fixed bottom-0 left-0 flex h-[104px] w-full justify-center'>
+    <div className='fixed bottom-0 left-0 flex h-[104px] w-full justify-center bg-apply-changes'>
       <div className='flex w-full max-w-[1298px] flex-1 items-center justify-center gap-[26px] px-5'>
         <Button
           className='h-[64px] w-full items-center gap-2 bg-success text-[18px] hover:bg-success/80'
@@ -25,7 +25,7 @@ const ApplyChanges: FC<ApplyChangesProps> = ({
           <span> Save Changes</span>
         </Button>
         <Button
-          className='border-cancel-btn-border hover:bg-cancel-btn-border bg-apply-changes h-[64px] w-full border-4 text-[18px]'
+          className='h-[64px] w-full border-4 border-cancel-btn-border bg-apply-changes text-[18px] hover:bg-cancel-btn-border'
           onClick={handleCancelChanges}>
           Cancel
         </Button>
